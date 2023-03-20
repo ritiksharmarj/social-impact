ee5d53573cb09dec7eab0356c6962b709a58e449
ritiksharmarj <theonlyritik@gmail.com>
2023-03-20 21:56:07 +0530

    Fixing error for complaint page

--- src/pages/Complaint.js
+++ src/pages/Complaint.js
@@ -8,7 +8,7 @@ const Complaint = () => {
           {/* Upload file */}
           <div className='flex items-center justify-center w-full'>
             <label
-              for='upload-file'
+              htmlFor='upload-file'
               className='flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600'
             >
               <div className='flex flex-col items-center justify-center pt-5 pb-6'>
@@ -21,9 +21,7 @@ const Complaint = () => {
                   xmlns='http://www.w3.org/2000/svg'
                 >
                   <path
-                    stroke-linecap='round'
-                    stroke-linejoin='round'
-                    stroke-width='1.5'
+                    strokeWidth='1.5'
                     d='M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12'
                   ></path>
                 </svg>
@@ -35,22 +33,22 @@ const Complaint = () => {
                   SVG, PNG, JPG or GIF (MAX. 800x400px)
                 </p>
               </div>
-              <input id='upload-file' type='file' class='hidden' required />
+              <input id='upload-file' type='file' className='hidden' required />
             </label>
           </div>
 
           {/* Description */}
           <div>
             <label
-              for='message'
-              class='block mb-2 text font-medium text-gray-900 dark:text-white'
+              htmlFor='message'
+              className='block mb-2 text font-medium text-gray-900 dark:text-white'
             >
               Description
             </label>
             <textarea
               id='description'
               rows='4'
-              class='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 outline-none'
+              className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 outline-none'
               placeholder='Write your thoughts here...'
             ></textarea>
           </div>
@@ -58,17 +56,17 @@ const Complaint = () => {
           {/* Catgories */}
           <div>
             <label
-              for='categories'
+              htmlFor='categories'
               className='block mb-2 font-medium text-gray-900 dark:text-white'
             >
               Select a category
             </label>
             <select
-              required
               id='categories'
               className='bg-gray-50 border border-gray-300 text-gray-600 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              required
             >
-              <option selected>Choose a category</option>
+              <option defaultValue>Choose a category</option>
               <option value='environment'>Environment</option>
               <option value='crime'>Crime</option>
               <option value='animal-safety'>Animal Safety</option>
@@ -78,7 +76,7 @@ const Complaint = () => {
           {/* Location */}
           <div>
             <label
-              for='complaint-location'
+              htmlFor='complaint-location'
               className='block mb-2 font-medium text-gray-900 dark:text-white'
             >
               Add location
@@ -96,7 +94,7 @@ const Complaint = () => {
             type='submit'
             className='text-white bg-blue-500 hover:bg-blue-600 transition rounded-full w-full py-2 px-4 mt-5'
           >
-            Submit
+            Post complaint
           </button>
         </form>
       </div>
