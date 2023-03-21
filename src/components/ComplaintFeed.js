21d80c8b63f12abe0656accfcec96b9b45e2e498
ritiksharmarj <theonlyritik@gmail.com>
2023-03-21 08:46:13 +0530

    Fixes bugs

--- src/components/ComplaintFeed.js
+++ src/components/ComplaintFeed.js
@@ -5,14 +5,33 @@ import React from 'react';
 import ComplaintCard from './cards/ComplaintCard';
 
 const ComplaintFeed = ({ complaintUserData, setComplaintUserData }) => {
-  complaintUserData = complaintUserData.sort(function(a, b){return Date.parse(a.created_at) - Date.parse(b.created_at)})
+  // setComplaintUserData(sort_by_date(complaintUserData));
+
+  sort_by_date(complaintUserData);
+  console.log(sort_by_date(complaintUserData));
+
+  function sort_by_date(array) {
+    // Copy the array to avoid mutating the original
+    let sorted = [...array];
+    // Use the built-in sort method with a custom comparator function
+    sorted.sort((a, b) => {
+      // Compare the create_at properties of two objects
+      return b.complaint_id - a.complaint_id;
+    });
+    // Return the sorted array
+    return sorted;
+  }
+
   return (
     <>
       <div className='mx-auto max-w-3xl'>
         <div className='px-4 sm:px-6 py-6 flex flex-col gap-5'>
           {complaintUserData[0] &&
             complaintUserData[0].map((singleData) => (
-              <ComplaintCard key={singleData.user_id} singleData={singleData} />
+              <ComplaintCard
+                key={singleData.complaint_id}
+                singleData={singleData}
+              />
             ))}
         </div>
       </div>
--- src/pages/Complaint.js
+++ src/pages/Complaint.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import { supabase } from "../supabase";
-import {Circles} from 'react-loader-spinner';
+import React, { useState } from 'react';
+import { supabase } from '../supabase';
+import { Circles } from 'react-loader-spinner';
 
 const Complaint = () => {
-  const [fileName, setFileName] = useState("");
-  const [fileUrl, setFileUrl] = useState("");
+  const [fileName, setFileName] = useState('');
+  const [fileUrl, setFileUrl] = useState('');
   const [loading, setLoading] = useState(false);
 
   const [data, setData] = useState({});
@@ -28,18 +28,24 @@ const Complaint = () => {
   };
 
   const submit = (e) => {
+    console.log(e);
     e.preventDefault();
-    if(fileUrl !== ''){
-      delete data["upload-file"]
-      data["media"] = fileUrl;
-      data["user_id"] = localStorage.getItem('loggedIn_user_id') != null 
-                                      ? localStorage.getItem('loggedIn_user_id') : 'ritik_sharma';
+    if (fileUrl !== '') {
+      delete data['upload-file'];
+      data['media'] = fileUrl;
+      data['user_id'] =
+        localStorage.getItem('loggedIn_user_id') != null
+          ? localStorage.getItem('loggedIn_user_id')
+          : 'ritik_sharma';
       console.log(data);
 
-      supabase.from('complaintDetails').insert(data)
-      .then((res) => {
-        console.log(res);
-      })
+      supabase
+        .from('complaintDetails')
+        .insert(data)
+        .then((res) => {
+          console.log(res);
+          window.location.pathname = '';
+        });
     }
   };
 
@@ -47,73 +53,85 @@ const Complaint = () => {
     // upload the image file to mediabucket/complaints folder
     setLoading(true);
     supabase.storage
-      .from("mediabucket")
-      .upload("complaints/" + e.target.files[0].name, e.target.files[0], {
-        cacheControl: "3600",
+      .from('mediabucket')
+      .upload('complaints/' + e.target.files[0].name, e.target.files[0], {
+        cacheControl: '3600',
         upsert: true,
       })
       .then((res) => {
         // handle data and error
         if (res && res.data && res.data.path) {
-          console.log("Upload successful!");
+          console.log('Upload successful!');
           setLoading(false);
-          setFileUrl(supabase.storage.from("mediabuckets").getPublicUrl(res.data.path).data.publicUrl);
-        } 
-      }).catch((err) => {
+          setFileUrl(
+            supabase.storage.from('mediabucket').getPublicUrl(res.data.path)
+              .data.publicUrl
+          );
+        }
+      })
+      .catch((err) => {
         console.error(err.message);
       });
   };
 
   return (
-    <div className="mx-auto max-w-3xl">
-      <div className="px-4 sm:px-6 py-10">
+    <div className='mx-auto max-w-3xl'>
+      <div className='px-4 sm:px-6 py-10'>
         <form
-          action=""
+          action=''
           onSubmit={submit}
-          name="complaint-form"
-          id="complaint-form"
-          className="flex flex-col gap-5"
+          name='complaint-form'
+          id='complaint-form'
+          className='flex flex-col gap-5'
         >
           {/* Upload file */}
-          <div className="flex items-center justify-center w-full">
+          <div className='flex items-center justify-center w-full'>
             <label
               htmlFor='upload-file'
               className='flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 '
             >
-              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+              <div className='flex flex-col items-center justify-center pt-5 pb-6'>
                 <svg
-                  aria-hidden="true"
-                  className="w-10 h-10 mb-3 text-gray-400"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden='true'
+                  className='w-10 h-10 mb-3 text-gray-400'
+                  fill='none'
+                  stroke='currentColor'
+                  viewBox='0 0 24 24'
+                  xmlns='http://www.w3.org/2000/svg'
                 >
                   <path
                     strokeWidth='1.5'
                     d='M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12'
                   ></path>
                 </svg>
-                <p className="mb-2 text-sm text-gray-500">
-                  <span className="font-semibold">Click to upload</span> or drag
+                <p className='mb-2 text-sm text-gray-500'>
+                  <span className='font-semibold'>Click to upload</span> or drag
                   and drop
                 </p>
-                <p className="text-xs text-gray-500">
+                <p className='text-xs text-gray-500'>
                   SVG, PNG, JPG or GIF (MAX. 800x400px)
                 </p>
               </div>
-              <input id='upload-file' type='file' className='hidden' required />
+              <input
+                onChange={handleUpload}
+                name='upload-file'
+                id='upload-file'
+                type='file'
+                className='hidden'
+                required
+              />
+              <p>{fileName}</p>
             </label>
           </div>
 
-          <div className="w-full flex items-center justify-center">
+          <div className='w-full flex items-center justify-center'>
             <Circles
-              height="40"
-              width="30"
-              color="dodgerblue"
-              ariaLabel="circles-loading"
+              height='40'
+              width='30'
+              color='dodgerblue'
+              ariaLabel='circles-loading'
               wrapperStyle={{}}
-              wrapperClass=""
+              wrapperClass=''
               visible={loading}
             />
           </div>
@@ -127,6 +145,8 @@ const Complaint = () => {
               Description
             </label>
             <textarea
+              name='description'
+              onChange={updateData}
               id='description'
               rows='4'
               className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 outline-none'
@@ -137,13 +157,15 @@ const Complaint = () => {
           {/* Catgories */}
           <div>
             <label
-              htmlFor='categories'
+              htmlFor='category'
               className='block mb-2 font-medium text-gray-900 '
             >
               Select a category
             </label>
             <select
-              id='categories'
+              name='category'
+              onChange={updateData}
+              id='category'
               className='bg-gray-50 border border-gray-300 text-gray-600 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 '
               required
             >
@@ -163,20 +185,19 @@ const Complaint = () => {
               Add location
             </label>
             <input
-              type="text"
-              id="location"
-              name="location"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-blue-500 block w-full p-2.5 outline-none"
-              placeholder="VT Road, Mansarovar, Rajasthan, India (302020)"
+              type='text'
+              id='location'
+              name='location'
+              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-blue-500 block w-full p-2.5 outline-none'
+              placeholder='VT Road, Mansarovar, Rajasthan, India (302020)'
               required
               onChange={updateData}
             />
           </div>
 
           <button
-            type="submit"
-            disabled={loading}
-            className="text-white bg-blue-500 hover:bg-blue-600 transition rounded-full w-full py-2 px-4 mt-5"
+            type='submit'
+            className='text-white bg-blue-500 hover:bg-blue-600 transition rounded-full w-full py-2 px-4 mt-5'
           >
             Post complaint
           </button>
